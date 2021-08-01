78888976fe87544bdee84614cc232d98f9898720
Your Name <you@example.com>
2021-08-01 19:12:30 +0300

    deploy_2

--- src/components/Content/StudentInf/StudentLessons/StudentTable.jsx
+++ src/components/Content/StudentInf/StudentLessons/StudentTable.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import './StudentTable.scss';
 
-const StudentTable = ({lessons, setLessons}) => {
-  // setLessons();
-  // let lessons = [
-  //     { id: 0, name: 'Урок 1', amountOfExercises: '100', amountOfDoneExercises: '90', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 1, name: 'Урок 2', amountOfExercises: '30', amountOfDoneExercises: '25', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 2, name: 'Урок 3', amountOfExercises: '70', amountOfDoneExercises: '70', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 3, name: 'Урок 4', amountOfExercises: '20', amountOfDoneExercises: '10', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 3, name: 'Урок 5', amountOfExercises: '33', amountOfDoneExercises: '27', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 4, name: 'Урок 6', amountOfExercises: '23', amountOfDoneExercises: '19', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 5, name: 'Урок 7', amountOfExercises: '55', amountOfDoneExercises: '50', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 6, name: 'Урок 8', amountOfExercises: '95', amountOfDoneExercises: '10', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 7, name: 'Урок 9', amountOfExercises: '55', amountOfDoneExercises: '50', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  //     { id: 8, name: 'Урок 10', amountOfExercises: '95', amountOfDoneExercises: '10', amountOfMistakes: '19', amountOfInaccuracies: '33', spentTime: '100', earnedTerricoins: '1000' },
-  // ]
+const StudentTable = ({lessons}) => {
 
   let tablesColumnsNames = [
     '№',
